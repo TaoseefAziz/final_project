f48a7032bbf3293f866352b3de697dbacad3b719
TaoseefAziz <taoseefaziz@arizona.edu>
2024-04-07 16:54:01 -0700

    CSV fully filled

--- scraping_gsm_arena_1_device.py
+++ scraping_gsm_arena_1_device.py
@@ -142,15 +142,17 @@ if __name__ == "__main__":
 
     devices_new_df = pd.read_csv(DEVICE_URLS_FILE)
     all_columns=["manufacturer","device","url","Network_Technology","Network_2G bands","Network_GPRS",
-    "Network_EDGE","Launch_Announced","Launch_Status","Body_Dimensions","Body_Weight","Body_SIM","Body_ ","Display_Type","Display_Size",
+    "Network_EDGE","Launch_Announced","Launch_Status","Body_Dimensions","Body_Weight","Body_SIM","Body_","Display_Type","Display_Size",
     "Display_Resolution","Platform_OS","Platform_Chipset","Platform_CPU","Memory_Card slot","Memory_Internal","Main Camera_Single",
     "Main Camera_Video","Selfie camera_Single","Selfie camera_Video","Sound_Loudspeaker ","Sound_3.5mm jack ","Comms_WLAN","Comms_Bluetooth",
     "Comms_Positioning","Comms_NFC","Comms_Radio","Comms_USB","Features_Sensors","Battery_Type","Battery_Talk time","Misc_Colors","Misc_Price",
-    "Network_3G bands","Network_4G bands","Network_ ","Network_Speed","Platform_GPU","Main Camera_Features","Memory_ ","Misc_Models","Body_Build",
+    "Network_3G bands","Network_4G bands","Network_","Network_Speed","Platform_GPU","Main Camera_Features","Memory_","Misc_Models","Body_Build",
     "Main Camera_Triple","Selfie camera_Features"]
 
+    start = 1491
     for index, row in devices_new_df.iterrows():
-        specifications_df =  pd.DataFrame(columns=all_columns)
-        specs_dict = get_specifications_for_device(row['manufacturer'],row['device'],row['url'])
-        specifications_df.loc[0] = specs_dict
-        specifications_df.to_csv(SPECIFICATIONS_FILE, mode = "a",index=False, header = False)
+        if index > start:
+            specifications_df =  pd.DataFrame(columns=all_columns)
+            specs_dict = get_specifications_for_device(row['manufacturer'],row['device'],row['url'])
+            specifications_df.loc[0] = specs_dict
+            specifications_df.to_csv(SPECIFICATIONS_FILE, mode = "a",index=False, header = False)
