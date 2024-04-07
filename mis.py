166737155ad3f7491b384165eeaccc858892206d
TaoseefAziz <taoseefaziz@arizona.edu>
2024-04-07 16:04:58 -0700

    Removed garbage files

--- mis.py
+++
@@ -1,5 +0,0 @@
-def correct_spelling(input_file, spell_dict):
-    file = open("test.txt",'w')
-    file.write("hello\nhow\nare\nyou\n")
-    file.close()
-correct_spelling("", {})
--- test.py
+++
@@ -1,5 +0,0 @@
-num = 7.888989
-num2 = 0.0789
-string = "key"
-print("{:14s}{:6.2f}%zz".format(string,num))
-print("{:14s}{:6.2%}zz".format(string,num2))
