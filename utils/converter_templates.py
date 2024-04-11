8c3f3e58d15d97d1a84efc6af6f9d86a24efb55d
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-04-11 16:34:37 -0700

    [fix] fix the docstring for replace_column

--- utils/converter_templates.py
+++ utils/converter_templates.py
@@ -6,14 +6,13 @@ import typing
 def replace_column(df: pd.DataFrame, column: str,
                    **new_cols_and_funcs: typing.Callable[[str], typing.Any]) -> pd.DataFrame:
     """
-    Replace column with one or more columns, each with a value extracted via regex from the values of the
-    original column
+    Replace column with one or more columns, each with a value extracted from the values of the original column
 
     :param df: the DataFrame
     :param column: the column to replace
     :param new_cols_and_funcs: keyword arguments, one for each new column. For each column, the column name
-        will be the argument's name, and the values will be extracted from the original column using the regex
-        specified by the argument's value.
+        will be the argument's name, and the values will be extracted from the original column using the
+        function specified by the argument's value.
     :return: a copy of the DataFrame after the operation
     """
 
