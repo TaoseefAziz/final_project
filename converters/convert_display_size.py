d0caa9216a117ba75148522a820b6d271da2e6d7
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-04-11 16:39:31 -0700

    some renames

--- converters/convert_display_size.py
+++ converters/convert_display_size.py
@@ -3,9 +3,9 @@ from utils import regex, converter_templates
 
 
 def convert_display_size(df: pd.DataFrame) -> pd.DataFrame:
-    screen_size_regex = regex.extractor_generator(r"(\d+\.\d+) inches")
-    area_regex = regex.extractor_generator(r"(\d+\.\d+) cm2")
-    ratio_regex = regex.extractor_generator(r"\(~(\d+\.\d+)% screen-to-body ratio\)")
+    size_extractor = regex.extractor_generator(r"(\d+\.\d+) inches")
+    area_extractor = regex.extractor_generator(r"(\d+\.\d+) cm2")
+    ratio_extractor = regex.extractor_generator(r"\(~(\d+\.\d+)% screen-to-body ratio\)")
 
-    return converter_templates.replace_column(df, "Display_Size", screen_size=screen_size_regex,
-                                              area=area_regex, ratio=ratio_regex)
+    return converter_templates.replace_column(df, "Display_Size", screen_size=size_extractor,
+                                              area=area_extractor, ratio=ratio_extractor)
