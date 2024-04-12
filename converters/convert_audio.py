2799616cc54786f2c46719cb976314507752b904
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-04-11 17:11:21 -0700

    [ft] jack conversion + improvements to replace_column

---
+++ converters/convert_audio.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from utils import converter_templates
+
+
+def convert_jack(df: pd.DataFrame) -> pd.DataFrame:
+    def jack_bool(val: str):
+        if val == "Yes" or val == "No":
+            return val == "Yes"
+
+        return None
+
+    return converter_templates.replace_column(df, "Sound_3.5mm jack ", has_jack=jack_bool)
--- utils/converter_templates.py
+++ utils/converter_templates.py
@@ -3,13 +3,15 @@ from utils import regex
 import typing
 
 
-def replace_column(df: pd.DataFrame, column: str,
+def replace_column(df: pd.DataFrame, column: str, drop_original: bool = True, drop_new_nas: bool = True,
                    **new_cols_and_funcs: typing.Callable[[str], typing.Any]) -> pd.DataFrame:
     """
     Replace column with one or more columns, each with a value extracted from the values of the original column
 
     :param df: the DataFrame
     :param column: the column to replace
+    :param drop_original: whether to drop the original column
+    :param drop_new_nas: whether to drop rows that have a value of N/A in any of the new columns
     :param new_cols_and_funcs: keyword arguments, one for each new column. For each column, the column name
         will be the argument's name, and the values will be extracted from the original column using the
         function specified by the argument's value.
@@ -22,4 +24,12 @@ def replace_column(df: pd.DataFrame, column: str,
     for new_col, new_col_func in new_cols_and_funcs.items():
         new_cols_and_funcs[new_col] = extracted_column.map(new_col_func)
 
-    return df.assign(**new_cols_and_funcs).drop(columns=[column]).dropna(subset=list(new_cols_and_funcs))
+    new_df = df.assign(**new_cols_and_funcs)
+
+    if drop_original:
+        new_df.drop(columns=[column], inplace=True)
+
+    if drop_new_nas:
+        new_df.dropna(subset=list(new_cols_and_funcs), inplace=True)
+
+    return new_df
