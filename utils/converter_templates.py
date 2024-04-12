f44b06390ce66c8a4adebfb5d25e58bc4d210903
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-04-11 17:15:52 -0700

    we do a little reformatting

--- utils/converter_templates.py
+++ utils/converter_templates.py
@@ -1,21 +1,19 @@
 import pandas as pd
-from utils import regex
 import typing
 
 
 def convert_column(df: pd.DataFrame, column: str, drop_original: bool = True, drop_new_nas: bool = True,
                    **new_cols_and_funcs: typing.Callable[[str], typing.Any]) -> pd.DataFrame:
     """
-    Convert a column into one or more columns, each with a value extracted from the values of the original
-    column
+    Convert a column into one or more columns, each with a value extracted from the values of the original column
 
     :param df: the DataFrame
     :param column: the column to convert
     :param drop_original: whether to drop the original column
     :param drop_new_nas: whether to drop rows that have a value of N/A in any of the new columns
-    :param new_cols_and_funcs: keyword arguments, one for each new column. For each column, the column name
-        will be the argument's name, and the values will be extracted from the original column using the
-        function specified by the argument's value.
+    :param new_cols_and_funcs: keyword arguments, one for each new column. For each column, the column name will be the
+        argument's name, and the values will be extracted from the original column using the function specified by the
+        argument's value.
     :return: a copy of the DataFrame after the operation
     """
 
