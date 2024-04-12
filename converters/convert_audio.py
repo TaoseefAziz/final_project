ab69d0309e442e1328316e7272f62f5e5c1b58eb
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-04-11 17:35:49 -0700

    [ft] loudspeaker stuff

--- converters/convert_audio.py
+++ converters/convert_audio.py
@@ -5,8 +5,31 @@ from utils import converter_templates
 def convert_jack(df: pd.DataFrame) -> pd.DataFrame:
     def jack_bool(val: str):
         if val == "Yes" or val == "No":
-            return val == "Yes"
+            return int(val == "Yes")
 
         return None
 
     return converter_templates.convert_column(df, "Sound_3.5mm jack ", has_jack=jack_bool)
+
+
+def convert_loudspeaker(df: pd.DataFrame) -> pd.DataFrame:
+    def loudspeaker_bool(val: str):
+        if val.startswith("Yes") or val.startswith("No"):
+            return int(val.startswith("Yes"))
+
+        return None
+
+    def stereo_bool(val: str):
+        if val.startswith("Yes") or val.startswith("No"):
+            return int("stereo speakers" in val)
+
+        return None
+
+    def dual_bool(val: str):
+        if val.startswith("Yes") or val.startswith("No"):
+            return int("dual speakers" in val)
+
+        return None
+
+    return converter_templates.convert_column(df, "Sound_Loudspeaker", has_loudspeaker=loudspeaker_bool,
+                                              has_stereo=stereo_bool, has_dual=dual_bool)
